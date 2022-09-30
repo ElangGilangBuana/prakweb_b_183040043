0e5e8963214a04a06deaefb3cfc0429132456f77
Elang Gilang Buana <buana.183040043@mail.unpas.ac.id>
2022-09-30 15:38:45 +0700

    kedua

--- pw/pw/index.php
+++ pw/pw/index.php
@@ -6,25 +6,24 @@ if( !isset($_SESSION["login"]) ) {
     exit;
 } */
 
-// require 'functions.php';
+require 'functions.php';
 
-// // pagination
-// // konfigurasi
-// $jumlahDataPerHalaman = 5;
-// $jumlahData = count(query("SELECT * FROM buku"));
-// $jumlahHalaman = ceil($jumlahData / $jumlahDataPerHalaman);
-// $halamanAktif = ( isset($_GET["halaman"]) ) ? $_GET["halaman"] : 1;
-// $awalData = ( $jumlahDataPerHalaman *$halamanAktif ) - $jumlahDataPerHalaman;
+// pagination
+// konfigurasi
+$jumlahDataPerHalaman = 5;
+$jumlahData = count(query("SELECT * FROM buku"));
+$jumlahHalaman = ceil($jumlahData / $jumlahDataPerHalaman);
+$halamanAktif = (isset($_GET["halaman"])) ? $_GET["halaman"] : 1;
+$awalData = ($jumlahDataPerHalaman * $halamanAktif) - $jumlahDataPerHalaman;
 
 
-// $buku = query("SELECT * FROM buku LIMIT $awalData, $jumlahDataPerHalaman");
+$buku = query("SELECT * FROM buku LIMIT $awalData, $jumlahDataPerHalaman");
 
-// // tombol cari ditekan
-// if( isset($_POST["cari"]) ) {
-//     $buku = cari($_POST["keyword"]);
-// }
+// tombol cari ditekan
+if (isset($_POST["cari"])) {
+    $buku = cari($_POST["keyword"]);
+}
 
-// 
 ?>
 
 
@@ -46,13 +45,13 @@ if( !isset($_SESSION["login"]) ) {
 </head>
 
 <body>
-    <center>
-        <h1>DAFTAR MAHASISWA</h1>
-    </center>
+
+    <h1>DAFTAR MAHASISWA</h1>
+
     <!-- navbar -->
     <div class="jumbotron jumbotron-fluid">
         <div class="container text-center">
-            <a href="index.php" class="brand-logo"><img src="https://github.com/ElangGilangBuana/prakweb_b_183040043.git" width="300"></a>
+            <a href="index.php" class="brand-logo"><img src="img/logo.png" width="300"></a>
         </div>
     </div>
 
@@ -80,47 +79,47 @@ if( !isset($_SESSION["login"]) ) {
             <th>Gambar</th>
         </tr>
         <?php $i = 1; ?>
-        <!-- <?php foreach ($buku as $bu) : ?>
-    <tr>
-        <td><?= $i ?></td>
-        <td>
-            <a href="ubah.php?id=<?= $bu["id"]; ?>">ubah</a>    |
-            <a href="hapus.php?id=<?= $bu["id"]; ?>" onclick="return confirm('yakin?');">hapus</a>
-        </td>
-        <td><?= $bu["kode_buku"]; ?></td>
-        <td><?= $bu["judul_buku"]; ?></td>
-        <td><?= $bu["penulis_buku"]; ?></td>
-        <td><?= $bu["harga"]; ?></td>
-        <td><img src="img/<?= $bu["gambar"]; ?>" width="150"></td>
-    </tr>
-    <?php $i++; ?>
-    <?php endforeach; ?> -->
+        <?php foreach ($buku as $bu) : ?>
+            <tr>
+                <td><?= $i ?></td>
+                <td>
+                    <a href="ubah.php?id=<?= $bu["id"]; ?>">ubah</a> |
+                    <a href="hapus.php?id=<?= $bu["id"]; ?>" onclick="return confirm('yakin?');">hapus</a>
+                </td>
+                <td><?= $bu["kode_buku"]; ?></td>
+                <td><?= $bu["judul_buku"]; ?></td>
+                <td><?= $bu["penulis_buku"]; ?></td>
+                <td><?= $bu["harga"]; ?></td>
+                <td><img src="img/<?= $bu["gambar"]; ?>" width="150"></td>
+            </tr>
+            <?php $i++; ?>
+        <?php endforeach; ?>
 
-        <!-- </table>
+    </table>
 
 
-<!-- navigasi -->
-        <div class="container text-center">
-            <?php if ($halamanAktif > 1) : ?>
-                <a href="?halaman=<?= $halamanAktif - 1; ?>">&laquo</a>
+    <!-- navigasi -->
+    <div class="container text-center">
+        <?php if ($halamanAktif > 1) : ?>
+            <a href="?halaman=<?= $halamanAktif - 1; ?>">&laquo</a>
+        <?php endif; ?>
+
+        <?php for ($i = 1; $i <= $jumlahHalaman; $i++) : ?>
+            <?php if ($i == $halamanAktif) : ?>
+                <a href="?halaman=<?= $i; ?>" style="font-weight: bold; color: red;"><?= $i; ?></a>
+            <?php else : ?>
+                <a href="?halaman=<?= $i; ?>"><?= $i; ?></a>
             <?php endif; ?>
 
-            <?php for ($i = 1; $i <= $jumlahHalaman; $i++) : ?>
-                <?php if ($i == $halamanAktif) : ?>
-                    <a href="?halaman=<?= $i; ?>" style="font-weight: bold; color: red;"><?= $i; ?></a>
-                <?php else : ?>
-                    <a href="?halaman=<?= $i; ?>"><?= $i; ?></a>
-                <?php endif; ?>
+        <?php endfor; ?>
 
-            <?php endfor; ?>
+        <?php if ($halamanAktif < $jumlahHalaman) : ?>
+            <a href="?halaman=<?= $halamanAktif + 1; ?>">&raquo</a>
+        <?php endif; ?>
+    </div>
+    <br><br>
 
-            <?php if ($halamanAktif < $jumlahHalaman) : ?>
-                <a href="?halaman=<?= $halamanAktif + 1; ?>">&raquo</a>
-            <?php endif; ?>
-        </div> -->
-        <br><br>
-
-        <!--
+    <!--
 <div class="container text-center">
 <a href="logout.php" class="btn btn-primary">Logout</a>
 </div>
@@ -128,13 +127,13 @@ if( !isset($_SESSION["login"]) ) {
 
 
 
-        <!-- Optional JavaScript; choose one of the two! -->
+    <!-- Optional JavaScript; choose one of the two! -->
 
-        <!-- Option 1: Bootstrap Bundle with Popper -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
 
-        <!-- Option 2: Separate Popper and Bootstrap JS -->
-        <!--
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js" integrity="sha384-lpyLfhYuitXl2zRZ5Bn2fqnhNAKOAaM/0Kr9laMspuaMiZfGmfwRNFh8HlMy49eQ" crossorigin="anonymous"></script>
     -->
