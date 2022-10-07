ba56b2935326edf45606ec2c75801e2d980da3eb
Elang Gilang Buana <buana.183040043@mail.unpas.ac.id>
2022-10-07 19:08:56 +0700

    Assets

---
+++ mvc/app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_b_183040043/mvc/public');
--- mvc/app/views/about/index.php
+++ mvc/app/views/about/index.php
@@ -1,2 +1,6 @@
-<h1>About Me</h1>
-<p>Halo, Nama saya <?= $data['nama']; ?>, Umur saya <?= $data['umur']; ?> Tahun, Saya adalah seorang <?= $data['pekerjaan']; ?></p>
+<div class="container">
+    <h1 class='mt-4'>About Me</h1>
+    <img src="<?= BASEURL ?>/img/el.jpg" alt="Elang Gilang Buana" width="200" class="rounded-circle shadow">
+    <p>Halo, Nama saya <?= $data['nama']; ?>, Umur saya <?= $data['umur']; ?> Tahun, Saya adalah seorang <?= $data['pekerjaan']; ?></p>
+
+</div>
